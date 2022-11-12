639a0a3b63ae60bd45d68dd1927262d002010803
김현진 <kimhyeonjin@gimhyeonjin-ui-MacBookPro.local>
2022-11-13 04:56:24 +0900

    feat : get Home view

--- Backend/haruject/src/main/java/com/umc/haruject/src/home/HomeController.java
+++ Backend/haruject/src/main/java/com/umc/haruject/src/home/HomeController.java
@@ -2,14 +2,21 @@ package com.umc.haruject.src.home;
 
 
 
+import com.umc.haruject.config.BaseException;
+import com.umc.haruject.config.BaseResponse;
+import com.umc.haruject.src.home.model.GetHomeRes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/")
+@RequestMapping("/home")
 public class HomeController {
     final Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -19,11 +26,26 @@ public class HomeController {
     @Autowired
     private final HomeService homeService;
 
-
     public HomeController(HomeProvider homeProvider, HomeService homeService) {
         this.homeProvider = homeProvider;
         this.homeService = homeService;
     }
+
+    /*
+    홈 화면 (게시글 목록 조회)
+     */
+    @ResponseBody
+    @GetMapping("")
+    public BaseResponse<List<GetHomeRes>> getHome(){
+        try {
+            List<GetHomeRes> getHomeRes = homeProvider.getHome();
+            return new BaseResponse<>(getHomeRes);
+        } catch (BaseException exception) {
+            return new BaseResponse<>(exception.getStatus());
+        }
+    }
+
+
 
 
 }
--- Backend/haruject/src/main/java/com/umc/haruject/src/home/HomeProvider.java
+++ Backend/haruject/src/main/java/com/umc/haruject/src/home/HomeProvider.java
@@ -1,16 +1,33 @@
 package com.umc.haruject.src.home;
 
+import com.umc.haruject.config.BaseException;
+import com.umc.haruject.src.home.model.GetHomeRes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Map;
+
+import static com.umc.haruject.config.BaseResponseStatus.*;
 
 @Service
 public class HomeProvider {
-
+    private final HomeDao homeDao;
 
     final Logger logger = LoggerFactory.getLogger(this.getClass());
+    public HomeProvider(HomeDao homeDao){
+        this.homeDao = homeDao;
+    }
 
+
+    public List<GetHomeRes> getHome() throws BaseException {
+        try{
+            List<GetHomeRes> getHomeRes = homeDao.selectPost();
+            return getHomeRes;
+        }
+        catch (Exception exception) {
+            //System.out.println(exception);
+            throw new BaseException(DATABASE_ERROR);
+        }
+    }
 }
