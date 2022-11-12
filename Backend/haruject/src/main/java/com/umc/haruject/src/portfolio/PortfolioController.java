79698e54a8a95e170f3afb684d085a79fc73b2e3
Dasom Ahn <lizzy0521@naver.com>
2022-11-13 04:46:59 +0900

    feat: portfolio/test/

--- Backend/haruject/src/main/java/com/umc/haruject/src/portfolio/PortfolioController.java
+++ Backend/haruject/src/main/java/com/umc/haruject/src/portfolio/PortfolioController.java
@@ -12,28 +12,33 @@ import java.awt.*;
 @RequestMapping("/portfolio")
 public class PortfolioController {
 
-    @Autowired
-    private final PortfolioProvider portfolioProvider;
+    @GetMapping("/test")
+    public String test() {
+        return "hello";
+    }
+
 //    @Autowired
-//    private final PortfolioService portfolioService;
-
-    public PortfolioController(PortfolioProvider portfolioProvider) { //, PortfolioService portfolioService) {
-        this.portfolioProvider = portfolioProvider;
-//        this.portfolioService = portfolioService;
-    }
-
-    // 개인 포폴 조회
-    @ResponseBody
-    @GetMapping("/userIdx")
-    public BaseResponse<GetPortRes> getUser(@PathVariable("idx") int idx) {
-        try {
-            GetPortRes getPortRes = portfolioProvider.getPortfolio(idx);
-            return new BaseResponse<>(getPortRes);
-        } catch (BaseException exception) {
-            return new BaseResponse<>((exception.getStatus()));
-        }
-
-    }
-
-    // 전체 포폴들 조회
+//    private final PortfolioProvider portfolioProvider;
+////    @Autowired
+////    private final PortfolioService portfolioService;
+//
+//    public PortfolioController(PortfolioProvider portfolioProvider) { //, PortfolioService portfolioService) {
+//        this.portfolioProvider = portfolioProvider;
+////        this.portfolioService = portfolioService;
+//    }
+//
+//    // 개인 포폴 조회
+//    @ResponseBody
+//    @GetMapping("/userIdx")
+//    public BaseResponse<GetPortRes> getUser(@PathVariable("idx") int idx) {
+//        try {
+//            GetPortRes getPortRes = portfolioProvider.getPortfolio(idx);
+//            return new BaseResponse<>(getPortRes);
+//        } catch (BaseException exception) {
+//            return new BaseResponse<>((exception.getStatus()));
+//        }
+//
+//    }
+//
+//    // 전체 포폴들 조회
 }
