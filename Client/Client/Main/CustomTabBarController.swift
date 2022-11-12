e979aa4490edce3e94d3b49937213db06cb6eee8
Hongjunehuke <junhyeok2166@daum.net>
2022-11-13 01:19:21 +0900

    fear: tabBar Viewcontroller

--- Client/Client/Main/CustomTabBarController.swift
+++ Client/Client/Main/CustomTabBarController.swift
@@ -11,12 +11,41 @@ import SnapKit
 
 class CustomTabBarController: UITabBarController{
     
+    fileprivate lazy var defaultTabBarHeight = { tabBar.frame.size.height }()
+    
+    let homeVC = HomeViewController()
+    let ScheduleVC = ScheduleViewController()
+    let profileVC = ProfileViewController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-
+        self.navigationItem.hidesBackButton = true
+        setUpTabBar()
     }
+    
 
+
+    func setUpTabBar(){
+        self.tabBar.tintColor = UIColor.black
+        self.tabBar.unselectedItemTintColor = .black
+        self.tabBar.backgroundColor = .systemBackground
+
+        homeVC.title = "Home"
+        ScheduleVC.title = "Subscribe"
+        profileVC.title = "Profile"
+
+        let ViewControllers:[UIViewController] = [homeVC,ScheduleVC,profileVC]
+        // assign view controllers to tab bar
+        self.setViewControllers(ViewControllers, animated: true)
+
+        homeVC.tabBarItem.image = UIImage(systemName: "house")
+        ScheduleVC.tabBarItem.image = UIImage(systemName: "command")
+        profileVC.tabBarItem.image = UIImage(systemName: "person")
+        
+        //네비게이션 뷰컨으로 푸쉬했을 때 밑에 바가 사라지지 않도록
+        self.hidesBottomBarWhenPushed = false
+        viewWillLayoutSubviews()
+    }
 }
 
 
